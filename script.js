0b0375e150707c399503de10c54c143e66249011
mekanikpro <mekanikprofesional@gmail.com>
2025-01-31 15:10:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const products = [
+    {
+        id: 1,
+        name: "Produk 1",
+        description: "Deskripsi produk 1.",
+        price: 10000,
+        image: "assets/images/produk1.jpg" // Ganti dengan path gambar yang sesuai
+    },
+    {
+        id: 2,
+        name: "Produk 2",
+        description: "Deskripsi produk 2.",
+        price: 20000,
+        image: "assets/images/produk2.jpg" // Ganti dengan path gambar yang sesuai
+    },
+    {
+        id: 3,
+        name: "Produk 3",
+        description: "Deskripsi produk 3.",
+        price:
+        
